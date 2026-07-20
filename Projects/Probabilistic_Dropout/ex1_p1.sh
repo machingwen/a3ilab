1ed4015edc2d8f831031f616f80afea0768738d3
a901 <40617780+a901@users.noreply.github.com>
2026-07-20 17:43:27 +0800

    Update sh

--- Projects/Probabilistic_Dropout/ex1_p1.sh
+++ Projects/Probabilistic_Dropout/ex1_p1.sh
@@ -3,6 +3,8 @@
 
 python main_gemma.py -load yes -train yes -recount yes -check yes -relu yes -data siqa_short -dropout_method linear -input input_model -output my_new_model  -lr 1e-5 
 
+python main_gemma.py -load yes -train yes -recount yes -check yes -relu yes -data siqa_short -dropout_method linear -input my_new_model -output my_new_model  -lr 1e-5 
+
 
 python eval_gemma.py --dropout yes --threshold 0.005  --sd 0 --original no --recount yes --model my_new_model --recount_data data_siqa
 lm_eval --model hf \
